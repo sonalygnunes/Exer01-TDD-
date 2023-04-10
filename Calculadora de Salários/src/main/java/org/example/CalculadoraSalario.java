9309099f97b627c5cc04d8927fc9860ca9343a21
Hiago Lacerda <86925401+Hiagod@users.noreply.github.com>
2023-04-10 17:38:27 -0300

    Melhoramento geral do código

--- Calculadora de Salários/src/main/java/org/example/CalculadoraSalario.java
+++ Calculadora de Salários/src/main/java/org/example/CalculadoraSalario.java
@@ -2,37 +2,40 @@ package org.example;
 
 public class CalculadoraSalario {
     public double calculaSalarioLiquido(Funcionario funcionario) {
+        validarFuncionario(funcionario);
+
         double salarioBase = funcionario.getSalarioBase();
-        double desconto = 0.0;
+        double desconto = calcularDesconto(funcionario.getCargo(), salarioBase);
 
-        if (funcionario.getCargo().equals("Desenvolvedor")) {
-            if (salarioBase >= 3000.0) {
-                desconto = salarioBase * 0.2;
-            } else {
-                desconto = salarioBase * 0.1;
-            }
-        } else if (funcionario.getCargo().equals("DBA")) {
-            if (salarioBase >= 2000.0) {
-                desconto = salarioBase * 0.25;
-            } else {
-                desconto = salarioBase * 0.15;
-            }
-
-        } else if (funcionario.getCargo().equals("Testador")) {
-            if (salarioBase >= 2000.0) {
-                desconto = salarioBase * 0.25;
-            } else {
-                desconto = salarioBase * 0.15;
-            }
-        } else if (funcionario.getCargo().equals("Gerente")) {
-            if (salarioBase >= 5000.0) {
-                desconto = salarioBase * 0.3;
-            } else {
-                desconto = salarioBase * 0.2;
-            }
-
-        }
         double salarioLiquido = salarioBase - desconto;
         return salarioLiquido;
+    }
+
+    private double calcularDesconto(String cargo, double salarioBase) {
+        switch (cargo) {
+            case "Desenvolvedor":
+                return salarioBase >= 3000.0 ? salarioBase * 0.2 : salarioBase * 0.1;
+            case "DBA":
+                return salarioBase >= 2000.0 ? salarioBase * 0.25 : salarioBase * 0.15;
+            case "Testador":
+                return salarioBase >= 2000.0 ? salarioBase * 0.25 : salarioBase * 0.15;
+            case "Gerente":
+                return salarioBase >= 5000.0 ? salarioBase * 0.30 : salarioBase * 0.20;
+            default:
+                throw new IllegalArgumentException("Cargo inválido: " + cargo);
+        }
+    }
+
+    private void validarFuncionario(Funcionario funcionario) {
+
+        String cargo = funcionario.getCargo();
+        String email = funcionario.getEmail();
+
+        if (!"Desenvolvedor".equals(cargo) && !"DBA".equals(cargo) && !"Testador".equals(cargo) && !"Gerente".equals(cargo)) {
+            throw new IllegalArgumentException("Cargo inválido: " + cargo);
+        }
+        else if (!email.contains("@")){
+            throw new IllegalArgumentException("Email inválido: " + cargo);
+        }
     }
 }
