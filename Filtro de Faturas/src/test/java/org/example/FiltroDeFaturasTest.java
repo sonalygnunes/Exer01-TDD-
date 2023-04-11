b0f8e4cc0725480e5cfd61868a9c470aee6da206
Sonaly Garcia <70299131+sonalygnunes@users.noreply.github.com>
2023-04-11 20:44:07 -0300

    Adicionando testes e classes utilizadas
    Adicionando testes para garantir os comportamentos esperados pela classe e criando esqueleto da classe (sem código, apenas a classe vazia)

---
+++ Filtro de Faturas/src/test/java/org/example/FiltroDeFaturasTest.java
@@ -0,0 +1,80 @@
+package org.example;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class FiltroFaturasTest {
+
+    @Test
+    public void testFiltroValorMenorQue2000() {
+        Fatura fatura1 = new Fatura("001", 1500, new Date(), new Cliente("Ana", new Date(), "SP"));
+        Fatura fatura2 = new Fatura("002", 3000, new Date(), new Cliente("Arisu", new Date(), "RJ"));
+        Fatura fatura3 = new Fatura("003", 5000, new Date(), new Cliente("Mario", new Date(), "SC"));
+        List<Fatura> faturas = new ArrayList<>();
+        faturas.add(fatura1);
+        faturas.add(fatura2);
+        faturas.add(fatura3);
+        FiltroFaturas filtro = new FiltroFaturas();
+        List<Fatura> faturasFiltradas = filtro.filtrar(faturas);
+        assertEquals(2, faturasFiltradas.size());
+        assertFalse(faturasFiltradas.contains(fatura1));
+        assertTrue(faturasFiltradas.contains(fatura2));
+        assertFalse(faturasFiltradas.contains(fatura3));
+    }
+
+    @Test
+    public void testFiltroValorEntre2000E2500EDataMenorQueUmMesAtras() {
+        Calendar data = Calendar.getInstance();
+        data.add(Calendar.MONTH, -1);
+        Fatura fatura1 = new Fatura("001", 2000, data.getTime(), new Cliente("Ana", new Date(), "SP"));
+        Fatura fatura2 = new Fatura("002", 2400, data.getTime(), new Cliente("Arisu", new Date(), "RJ"));
+        Fatura fatura3 = new Fatura("003", 2600, new Date(), new Cliente("Mario", new Date(), "SC"));
+        List<Fatura> faturas = new ArrayList<>();
+        faturas.add(fatura1);
+        faturas.add(fatura2);
+        faturas.add(fatura3);
+        FiltroFaturas filtro = new FiltroFaturas();
+        List<Fatura> faturasFiltradas = filtro.filtrar(faturas);
+        assertEquals(2, faturasFiltradas.size());
+        assertFalse(faturasFiltradas.contains(fatura1));
+        assertTrue(faturasFiltradas.contains(fatura2));
+        assertFalse(faturasFiltradas.contains(fatura3));
+    }
+
+    @Test
+    public void testFiltroValorEntre2500E3000EDataInclusaoClienteMenorQue2MesesAtras() {
+        Calendar data = Calendar.getInstance();
+        data.add(Calendar.MONTH, -2);
+        Fatura fatura1 = new Fatura("001", 2000, new Date(), new Cliente("Ana", data.getTime(), "SP"));
+        Fatura fatura2 = new Fatura("002", 2700, new Date(), new Cliente("Arisu", new Date(), "RJ"));
+        Fatura fatura3 = new Fatura("003", 2900, new Date(), new Cliente("MArio", new Date(), "SC"));
+        List<Fatura> faturas = new ArrayList<>();
+        faturas.add(fatura1);
+        faturas.add(fatura2);
+        faturas.add(fatura3);
+        FiltroFaturas filtro = new FiltroFaturas();
+        List<Fatura> faturasFiltradas;
+
+    }
+    @Test
+    public void testFiltroValorMaiorQue4000EPertenceARegiaoSul() {
+        Fatura fatura1 = new Fatura("001", 5000, new Date(), new Cliente("Arisu", new Date(), "SP"));
+        Fatura fatura2 = new Fatura("002", 4500, new Date(), new Cliente("Ana", new Date(), "SC"));
+        Fatura fatura3 = new Fatura("003", 3000, new Date(), new Cliente("Mario", new Date(), "RS"));
+        List<Fatura> faturas = new ArrayList<>();
+        faturas.add(fatura1);
+        faturas.add(fatura2);
+        faturas.add(fatura3);
+        FiltroFaturas filtro = new FiltroFaturas();
+        List<Fatura> faturasFiltradas = filtro.filtrar(faturas);
+        assertEquals(2, faturasFiltradas.size());
+        assertFalse(faturasFiltradas.contains(fatura1));
+        assertFalse(faturasFiltradas.contains(fatura2));
+        assertTrue(faturasFiltradas.contains(fatura3));
+    }
+
+}
+
