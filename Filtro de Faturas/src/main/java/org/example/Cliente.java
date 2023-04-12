dbc28dc17449776fbb18068e39fa5ef09f0d9a6e
sonalygnunes <sonaly.nunes@ccc.ufcg.edu.br>
2023-04-11 21:41:11 -0300

    Adicionando classe de informações básica do cliente

--- Filtro de Faturas/src/main/java/org/example/Cliente.java
+++ Filtro de Faturas/src/main/java/org/example/Cliente.java
@@ -1,4 +1,39 @@
 package org.example;
 
+import java.util.Date;
+
 public class Cliente {
+    private String nome;
+    private Date dataInclusao;
+    private String estado;
+
+    public Cliente(String nome, Date dataInclusao, String estado) {
+        this.nome = nome;
+        this.dataInclusao = dataInclusao;
+        this.estado = estado;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataInclusao() {
+        return dataInclusao;
+    }
+
+    public void setDataInclusao(Date dataInclusao) {
+        this.dataInclusao = dataInclusao;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
