574cec0c3c139328c352e34ed89f860ce59fbb66
sonalygnunes <sonaly.nunes@ccc.ufcg.edu.br>
2023-04-11 22:23:38 -0300

    Criando métodos básico do cliente e funcionalidade de filtrar para passar no primeiro teste, remoção de valores menores que 2000

--- Filtro de Faturas/src/main/java/org/example/Fatura.java
+++ Filtro de Faturas/src/main/java/org/example/Fatura.java
@@ -1,4 +1,49 @@
 package org.example;
 
+import java.util.Date;
+
 public class Fatura {
+    private String codigo;
+    private double valor;
+    private Date data;
+    private Cliente cliente;
+
+    public Fatura(String codigo, double valor, Date data, Cliente cliente) {
+        this.codigo = codigo;
+        this.valor = valor;
+        this.data = data;
+        this.cliente = cliente;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
 }
--- Filtro de Faturas/src/main/java/org/example/FiltroFaturas.java
+++ Filtro de Faturas/src/main/java/org/example/FiltroFaturas.java
@@ -1,4 +1,23 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class FiltroFaturas {
+    public FiltroFaturas() {
+    }
+    public List<Fatura> filtrar(List<Fatura> faturas) {
+
+        List<Fatura> faturasFiltradas = new ArrayList<>();
+
+        for (Fatura fatura : faturas) {
+            if (fatura.getValor() < 2000) {
+                continue;
+            }
+            faturasFiltradas.add(fatura);
+        }
+        return faturasFiltradas;
+    }
+
 }
